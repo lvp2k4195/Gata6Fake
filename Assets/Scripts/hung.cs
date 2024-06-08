e8be79170cf3c5e9052aff3df3662c0026f1c7e4
lvp2k4195 <130356940+lvp2k4195@users.noreply.github.com>
2024-06-08 23:32:30 +0700

    fff

--- Assets/Scripts/hung.cs
+++ Assets/Scripts/hung.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class h : MonoBehaviour
+public class hung : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
