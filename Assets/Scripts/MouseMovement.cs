9ab6fc03e71fa670cdaa6c01c7304c5df45574a7
luânps34917 <130386979+luanhuupuannn@users.noreply.github.com>
2024-06-08 22:34:01 +0700

    fix

--- Assets/Scripts/MouseMovement.cs
+++ Assets/Scripts/MouseMovement.cs
@@ -11,30 +11,31 @@ public class MouseMovement : MonoBehaviour
 
     public float topClamp = -90f;
     public float bottomClamp = 90f;
+
     // Start is called before the first frame update
     void Start()
     {
-        //Locking the cursor to the middle of the screen and making it invisible
+        // Locking the cursor to the middle of the screen and making it invisible
         Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-       //Getting the mouse inputs
-       float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-       float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        // Getting the mouse inputs with inversion for desired behavior
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        //Rotation around the x axis (Look up and down)
+        // Rotation around the x axis (Look up and down)
         xRotation -= mouseY;
 
-        //Clamp the rotation
+        // Clamp the rotation
         xRotation = Mathf.Clamp(xRotation, topClamp, bottomClamp);
 
-        //Rotation around the x axis (Look up and down)
-        yRotation -= mouseX;
+        // Rotation around the y axis (Look left and right)
+        yRotation += mouseX; // Use '+' for right-handed rotation
 
-        //Apply rotations to our transform
+        // Apply rotations to our transform
         transform.localRotation = Quaternion.Euler(xRotation, yRotation, 0f);
     }
 }
